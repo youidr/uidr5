5ddd047d69179897790eebec3f1732c97db9e632
youidr <idri@qq.com>
2017-11-14 20:40:59 +0800

    Update checkio.py

--- checkio.py
+++ checkio.py
@@ -7,3 +7,27 @@ def count_words(text, words):
         if re.search(a,text.lower()):
             c += 1
     return c
+
+def checkio1(game_result):
+    #三子棋比输赢
+    if game_result[0][0] == "X" and game_result[0][1] == "X" and game_result[0][2] == "X" \
+            or game_result[1][0] == "X" and game_result[1][1] == "X" and game_result[1][2] == "X" \
+            or game_result[2][0] == "X" and game_result[2][1] == "X" and game_result[2][2] == "X" \
+            or game_result[0][0] == "X" and game_result[1][0] == "X" and game_result[2][0] == "X" \
+            or game_result[0][1] == "X" and game_result[1][1] == "X" and game_result[2][1] == "X" \
+            or game_result[0][2] == "X" and game_result[1][2] == "X" and game_result[2][2] == "X" \
+            or game_result[0][0] == "X" and game_result[1][1] == "X" and game_result[2][2] == "X" \
+            or game_result[0][2] == "X" and game_result[1][1] == "X" and game_result[2][0] == "X":
+        return 'X'
+    elif game_result[0][0] == "O" and game_result[0][1] == "O" and game_result[0][2] == "O" \
+            or game_result[1][0] == "O" and game_result[1][1] == "O" and game_result[1][2] == "O" \
+            or game_result[2][0] == "O" and game_result[2][1] == "O" and game_result[2][2] == "O" \
+            or game_result[0][0] == "O" and game_result[1][0] == "O" and game_result[2][0] == "O" \
+            or game_result[0][1] == "O" and game_result[1][1] == "O" and game_result[2][1] == "O" \
+            or game_result[0][2] == "O" and game_result[1][2] == "O" and game_result[2][2] == "O" \
+            or game_result[0][0] == "O" and game_result[1][1] == "O" and game_result[2][2] == "O" \
+            or game_result[0][2] == "O" and game_result[1][1] == "O" and game_result[2][0] == "O":
+        return 'O'
+    else:
+        return "D"
+
