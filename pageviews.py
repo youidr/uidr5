ed8ba544c0773845b3e911ca58e446896643a533
youidr <idri@qq.com>
2017-11-20 23:53:54 +0800

    Update pageviews.py

--- pageviews.py
+++ pageviews.py
@@ -12,17 +12,52 @@ def getpages(v3,v4):
         v4 = v5
     v6 = v3[(v4 - 1) * showpage:v4 * showpage]
     return v6,v5,v3.count(),v4
+
+def test(p,c):
+    if p <= 3:
+        if c > 5:
+            a = [d for d in range(1,6)]
+        else:
+            a = [d for d in range(1,c+1)]
+    elif c - p <= 3:
+        a = [d for d in range(c-4,c+1)]
+    else:
+        a = [d for d in range(p-2,p+3)]
+    return a
     
-            if request.GET.get('p'):
+       if request.GET.get('p'):
             try:
                 p = int(request.GET.get('p'))
+                if p < 1:
+                    p = 1
             except:
                 p = 1
         else:
             p = 1
-        saps = getpages(getsqls(Serversum),p)
+        saps = getpages(getsqls(Serverslog),p)
         sqls = saps[0]
         pages = saps[1]
-        pagelist = [pa for pa in range(1,pages + 1)]
         sqlcount = saps[2]
         p = saps[3]
+        pagelist = test(p,pages)
+        if 1 < pagelist[0]:
+            previous_page = True
+            p_5 = p - 5
+            p_1 = p - 1
+        if pagelist[-1] < pages:
+            next_page = True
+            pa5 = p + 5
+            pa1 = p + 1
+            
+            
+                                    {% if previous_page %}
+                        <a href="?p={{ p_5 }}"><<</a> <a href="?p={{ p_1 }}"><</a>
+                        {% endif %}
+                        {% for pa in pagelist %}
+                            <a href="?p={{ pa }}">{{ pa }}</a>
+                        {% endfor %}
+                        {% if next_page %}
+                         <a href="?p={{ pa1 }}">></a> <a href="?p={{ pa5 }}">>></a>
+                        {% endif %}
+                        </td>
+                        <td>{{ p }}/{{ pages }}</td>
